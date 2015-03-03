0ac4c4db9120d8e15f6109394de924f99b683dcd
kkayed <kkayed@hsr.ch>
2015-03-03 18:47:47 +0100

    Test for tags

--- wear/src/main/java/ch/hsr/smartnaviwatch/MainActivity.java
+++ wear/src/main/java/ch/hsr/smartnaviwatch/MainActivity.java
@@ -62,7 +62,7 @@ public class MainActivity extends Activity {
                 {
                     case MessageTypes.MESSAGE_NEW_DIRECTION:
                         NewDirectionMessage msg = new NewDirectionMessage(e.getData());
-                        mTextView.setText("Phone message: " + msg.getMsg());
+                        mTextView.setText("Phone Message: " + msg.getMsg());
                         break;
                 }
             }
