1f7a6151b8cb59355660b5c7babdadb86c1f8315
kkayed <kkayed@hsr.ch>
2015-03-18 14:23:35 +0100

    master cleaned up

--- README.md
+++
@@ -1,3 +0,0 @@
-# SmartNaviWatch
-SA FS 2015
-[![Build Status](https://magnum.travis-ci.com/kkade/SmartNaviWatch.svg?token=5m6objVwpzUmpNHpZu3j&branch=master)](https://magnum.travis-ci.com/kkade/SmartNaviWatch)
--- transfer/src/main/java/ch/hsr/transfer/MessageTypes.java
+++
@@ -1,9 +0,0 @@
-package ch.hsr.transfer;
-
-/**
- * Created by Admin on 22.02.2015.
- */
-public final class MessageTypes {
-    public static final String MESSAGE_NEW_DIRECTION = "ch/hsr/msg/nav/dir";
-    public static final String MESSAGE_TELETEXT = "ch/hsr/msg/teletext";
-}
