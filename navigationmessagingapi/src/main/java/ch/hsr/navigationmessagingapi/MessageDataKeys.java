635be75f958b62b3f1b95ded4e78d561d0fa3325
kkayed <kkayed@hsr.ch>
2015-04-11 08:23:39 +0200

    - Constant values for message data keys - Message data is now passed via HashTable - Message type names revamped + new message types - Plugin now sends useful data in its messages

---
+++ navigationmessagingapi/src/main/java/ch/hsr/navigationmessagingapi/MessageDataKeys.java
@@ -0,0 +1,12 @@
+package ch.hsr.navigationmessagingapi;
+
+/**
+ * Constant values for keys of data passed in messages
+ */
+public final class MessageDataKeys {
+    public static final String TurnType = "TT";
+    public static final String TurnAngle = "A";
+    public static final String Distance = "D";
+    public static final String StreetName = "S";
+    public static final String RouteProgressPercentage = "P";
+}
--- navigationmessagingapi/src/main/java/ch/hsr/navigationmessagingapi/MessageTypes.java
+++ navigationmessagingapi/src/main/java/ch/hsr/navigationmessagingapi/MessageTypes.java
@@ -4,8 +4,9 @@ package ch.hsr.navigationmessagingapi;
  * Constants for message types
  */
 public final class MessageTypes {
-    public static final String NewRouteMessage = "ch/hsr/smartnaviwatch/nav/alert/newroute";
-    public static final String NextStepMessage = "ch/hsr/smartnaviwatch/nav/alert/step";
-    public static final String PositionMessage = "ch/hsr/smartnaviwatch/nav/quiet/location";
-    public static final String CancellationMessage = "ch/hsr/smartnaviwatch/nav/alert/cancelled";
+    public static final String NewRouteMessage = "ch/hsr/smartnaviwatch/alert/newroute";
+    public static final String NextStepMessage = "ch/hsr/smartnaviwatch/alert/step";
+    public static final String PositionMessage = "ch/hsr/smartnaviwatch/resp/location";
+    public static final String CancellationMessage = "ch/hsr/smartnaviwatch/alert/cancelled";
+    public static final String PositionRequest = "ch/hsr/smartnaviwatch/req/location";
 }
