ad4300fa7edb9e8986db5f247405625573d61118
Mirco <mircostraessle@hotmail.com>
2015-05-19 11:27:56 +0200

    Better text readability & location marker

--- wear/src/main/java/ch/hsr/smartnaviwatch/NavigationMain.java
+++ wear/src/main/java/ch/hsr/smartnaviwatch/NavigationMain.java
@@ -15,6 +15,7 @@ import android.view.animation.AnimationUtils;
 import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextClock;
 import android.widget.TextView;
 import android.widget.ViewFlipper;
 
